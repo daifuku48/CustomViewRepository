7cefcdefdb5c7f4954670a31e1f5d75cb51ae8a1
danil <natalya.haritonov.72@gmail.com>
2023-10-23 13:40:35 +0300

    custom linear layout

--- app/src/main/java/com/androismathhartdangame/customviewrepository/CustomLinearLayout.kt
+++ app/src/main/java/com/androismathhartdangame/customviewrepository/CustomLinearLayout.kt
@@ -4,13 +4,20 @@ import android.content.Context
 import android.util.AttributeSet
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.core.view.setPadding
 
 class CustomLinearLayout @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
 ) : LinearLayout(context, attrs, defStyleAttr) {
-    fun addItem(item: TextView) {
-        addView(item)
+    fun addItem(item: String) {
+        val textView = TextView(context)
+        textView.text = item
+        textView.textSize = 18f
+        textView.setBackgroundColor(resources.getColor(R.color.black, context.theme))
+        textView.setTextColor(resources.getColor(R.color.white, context.theme))
+        textView.setPadding(4)
+        addView(textView)
     }
 }
--- app/src/main/java/com/androismathhartdangame/customviewrepository/MainActivity.kt
+++ app/src/main/java/com/androismathhartdangame/customviewrepository/MainActivity.kt
@@ -2,16 +2,13 @@ package com.androismathhartdangame.customviewrepository
 
 import android.os.Bundle
 import com.androismathhartdangame.customviewrepository.databinding.ActivityMainBinding
-import com.androismathhartdangame.customviewrepository.databinding.ItemLayoutBinding
 
 class MainActivity: BaseActivity<ActivityMainBinding>() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         for (i in 0..15){
-            val itemBinding = ItemLayoutBinding.inflate(layoutInflater)
-            itemBinding.itemText.text = getString(R.string.item, i)
-            binding.customLinearLayout.addItem(itemBinding.itemText)
+            binding.customLinearLayout.addItem(getString(R.string.item, i))
         }
     }
 }
